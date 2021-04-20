ef968abf1531d270faac9a454ba66ed5a39589c4
riyagoel192 <riyagoel192@gmail.com>
2021-04-20 20:02:40 +0530

    navbar removed

--- project_ideas.php
+++ project_ideas.php
@@ -23,6 +23,7 @@ $total = $row[0];
 <html>
 <head>
 	<title>Project Ideas</title>
+	<link rel="stylesheet" href="Css/tablestyle.css">
 
 	<style>
 		a{
@@ -33,7 +34,23 @@ $total = $row[0];
 </head>
 <body>
 
-	<h1><center>INNOVATIVE PROJECT IDEAS</center></h1>
+	<div class="nav">
+		<!-- <ul>
+		 <li><a href="index.php">Home</a></li>
+		 <li><a href="about.html">Books</a></li>
+		 <li><a href="contact.html">Notes</a></li>
+		 <li><a href="contact.html">Previous Year </a></li>
+		 <li><a href="contact.html">Syllabus</a></li>
+		 <li><a href="contact.html">Practical</a></li>
+		 <li><a href="contact.html">Project Ideas</a></li>
+	 </ul> -->
+
+	 <h1>STUDYMATE</h1>
+
+	</div>
+	<br>
+
+	<h2><center>INNOVATIVE PROJECT IDEAS</center></h2>
 
 	<div class="container-fluid" style="height: 100%;">
 
@@ -54,7 +71,7 @@ $total = $row[0];
 					$link=$rows['project_link'];
 
 	
-					echo "<div class='col-md-4' style='float:left;margin: 3% 2%;border:1px solid black;border-width:2px;border-style:ridge;'>";
+					echo "<div class='col-md-4' style='float:left;margin: 3% 2%;box-shadow: 5px 10px 22px #888888;'>";
 
 					// echo "<div class='col-md-4' style='border:2px solid green;width:50%;height:10%;margin:2% 25%;'>";
 					// echo "<img src='https://previews.123rf.com/images/blackstork/blackstork1507/blackstork150700049/43766219-handwritten-page-of-draft-calculations.jpg' style='width:5%;height:10%;position:absolute;'></img>";
